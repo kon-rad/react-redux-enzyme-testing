a4a81fad8d74616605dd7e30dd446b029fdcbefc
konradgnat <konradmgnat@gmail.com>
2019-05-22 14:32:44 -0500

    flashcards

--- flashcardpro/src/components/Stack.js
+++ flashcardpro/src/components/Stack.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Card from './Card';
 
 class Stack extends Component {
 
   render() {
+    const { title, cards } = this.props;
     return (
       <div>
         <Link to="/">Home</Link>
-        <h3>title here</h3>
+        <h3>{title}</h3>
+        {
+          cards.map(card => {
+            <Card card={card} />
+          })
+        }
       </div>
     )
   }
